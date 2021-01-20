56895821e654a0ff43ead76d71b7d19af8cb347c
unknown <n0laja00@students.oamk.fi>
2021-01-20 10:29:01 +0200

    Finished example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBMI] = useState(0);
+
+
+    function handleSubmit(e) {
+      e.preventDefault();
+      const conversion = weight / (height*height);
+      setBMI(conversion.toFixed(1));
+    };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Currency calculator</h3>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>weight</label>
+          <input type="number" value={weight} onChange={e => setWeight(e.target.value)}></input>
+        </div>
+        <div>
+          <label>height</label>
+          <input type="number" value={height} onChange={e => setHeight(e.target.value)}></input>
+        </div>
+        <div>
+          <label>BMI </label>
+          <output>{bmi}</output>
+        </div>
+        <button>Calculate</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
